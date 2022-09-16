2046a090718bac6f2da9f41b443e7cca2f6246c7
qorina12 <qorinaprameswari12@gmail.com>
2022-09-16 18:57:52 +0700

    Add pdf report feature

---
+++ P31_Praktikum 3/formHandling/app/Models/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Article;
+
+class Article extends Model
+{
+    use HasFactory;
+     
+    protected $table = "articles";
+    
+    protected $fillable = [
+        'title',
+        'content',
+        'featured_image'
+    ];
+}
+
